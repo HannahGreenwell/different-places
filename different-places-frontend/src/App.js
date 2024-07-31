c77f96004584d99994f7624b0d891a13f71fbe2d
Hannah Greenwell <hannah.greenwell@gmail.com>
2024-07-31 17:14:02 +1000

    added prettier config

--- different-places-frontend/src/App.js
+++ different-places-frontend/src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
-import postcodes from "./data/postcodes.json";
-import "./App.css";
+import React from 'react';
+import postcodes from './data/postcodes.json';
+import './App.css';
 
 const getPostcode = () => {
   const { list } = postcodes;
--- different-places-frontend/src/App.test.js
+++ different-places-frontend/src/App.test.js
@@ -1,23 +1,23 @@
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import App from './App';
 
-test("renders Where to next? button", () => {
+test('renders Where to next? button', () => {
   render(<App />);
 
   expect(
-    screen.getByRole("button", { name: "Where to next?" })
+    screen.getByRole('button', { name: 'Where to next?' }),
   ).toBeInTheDocument();
 });
 
-test("renders postcode when user clicks Where to next? button", async () => {
+test('renders postcode when user clicks Where to next? button', async () => {
   render(<App />);
 
-  await userEvent.click(screen.getByRole("button", { name: "Where to next?" }));
+  await userEvent.click(screen.getByRole('button', { name: 'Where to next?' }));
 
   expect(screen.getByText(/3\d{3}/)).toBeInTheDocument();
   expect(
-    screen.queryByRole("button", { name: "Where to next?" })
+    screen.queryByRole('button', { name: 'Where to next?' }),
   ).not.toBeInTheDocument();
 });
