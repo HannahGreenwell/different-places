ebba91206e098f5e424a31e64fb901372cd662b7
Hannah Greenwell <hannah.greenwell@gmail.com>
2024-08-08 14:23:43 +1000

    added express-async-errors package

--- different-places-api/src/index.js
+++ different-places-api/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import 'express-async-errors';
 import logger from 'loglevel';
 import { router as placesRouter } from './routes/places';
 
@@ -16,15 +17,19 @@ const server = app.listen(PORT, () => {
   logger.info(`Coming at you live from port ${server.address().port}!`);
 });
 
-function errorMiddleware(error, req, res, next) {
-  logger.error(error);
-  res.status(500);
-  res.json({
-    message: error.message,
-    ...(process.env.NODE_ENV === 'production'
-      ? null
-      : {
-          stack: error.stack,
-        }),
-  });
+function errorMiddleware(err, req, res, next) {
+  if (res.headersSent) {
+    next(err);
+  } else {
+    logger.error(err);
+    res.status(500);
+    res.json({
+      message: err.message,
+      ...(process.env.NODE_ENV === 'production'
+        ? null
+        : {
+            stack: err.stack,
+          }),
+    });
+  }
 }
