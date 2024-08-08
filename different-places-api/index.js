c165ca4a95a448d3aaba45a1ff853b2bd6aeffac
Hannah Greenwell <hannah.greenwell@gmail.com>
2024-08-08 11:14:37 +1000

    added error middleware and logger

--- different-places-api/index.js
+++ different-places-api/index.js
@@ -1,1 +1,5 @@
-require('nodemon')({ script: 'dev.js' });
+if (process.env.NODE_ENV === 'production') {
+  require('./dist');
+} else {
+  require('nodemon')({ script: 'dev.js' });
+}
--- different-places-api/src/index.js
+++ different-places-api/src/index.js
@@ -1,12 +1,30 @@
 import express from 'express';
+import logger from 'loglevel';
 import { router as placesRouter } from './routes/places';
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+logger.setLevel('info');
+
 app.use('/api', placesRouter);
 
+app.use(errorMiddleware);
+
 const server = app.listen(PORT, () => {
-  console.log(`Coming at you live from port ${server.address().port}!`);
+  logger.info(`Coming at you live from port ${server.address().port}!`);
 });
+
+function errorMiddleware(error, req, res, next) {
+  logger.error(error);
+  res.status(500);
+  res.json({
+    message: error.message,
+    ...(process.env.NODE_ENV === 'production'
+      ? null
+      : {
+          stack: error.stack,
+        }),
+  });
+}
